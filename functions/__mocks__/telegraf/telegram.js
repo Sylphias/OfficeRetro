d040e53813f84c9d1003b30207042b7c535840f8
Koh Kai Wei <sylpharyon@gmail.com>
2019-12-24 21:47:28 +0800

    Add test for updateCallbackmessage, mocked telegraf/telegram class

--- functions/__mocks__/telegraf/telegram.js
+++ functions/__mocks__/telegraf/telegram.js
@@ -1,5 +1,14 @@
-const Telegram = jest.fn().mockImplementation(() => ({
-  editMessageReplyMarkup: jest.fn(),
-  editMessageText: jest.fn(),
+// class Telegram {
+//   constructor(config) {
+//     this.config = config;
+//     this.editMessageReplyMarkup = jest.fn();
+//     this.editMessageText = jest.fn();
+//   }
+// }
+const mockEditMessageReplyMarkup = jest.fn();
+const mockEditMessageText = jest.fn();
+const Telegram = jest.fn(() => ({
+  editMessageReplyMarkup: mockEditMessageReplyMarkup,
+  editMessageText: mockEditMessageText,
 }));
 module.exports = Telegram;
--- functions/__tests__/Helpers/CommandHelpers.js
+++ functions/__tests__/Helpers/CommandHelpers.js
@@ -2,13 +2,17 @@ const Telegram = require('telegraf/telegram');
 const CommandHelpers = require('../../Helpers/CommandHelpers');
 
 jest.mock('telegraf/telegram');
+const telegramClient = new Telegram();
 
 describe('Command Helpers should have operations that assist in carrying bot commands', () => {
+  beforeEach(() => {
+    telegramClient.editMessageReplyMarkup.mockClear();
+    telegramClient.editMessageText.mockClear();
+  });
   test('updateCallbackMessage should call telegramClient to update previous message',
-    () => {
-      CommandHelpers.updateCallbackMessage('111', '222', 'test');
-      console.log(Telegram);
-      expect(Telegram).toBeCalled();
-      expect(Telegram.editMessageReplyMarkup).toBeCalled();
+    async () => {
+      await CommandHelpers.updateCallbackMessage('111', '222', 'test');
+      expect(telegramClient.editMessageReplyMarkup).toBeCalledTimes(1);
+      expect(telegramClient.editMessageText).toBeCalledTimes(1);
     });
 });
